746999c909a3c05900ed3be775441a49b24188da
NahSama <hoangnguyendinh1107@gmail.com>
2020-09-06 03:16:46 +0800

    Updates

---
+++ precache-manifest.bd1ea2035e1767cd9e5180647ea8aae4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503a386cd69e318ba0c4b703fc04a0dd",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "3da7aa2c8244ed24d860",
+    "url": "/porfolio/static/css/2.2058ee32.chunk.css"
+  },
+  {
+    "revision": "c16ef27823cf51c2c752",
+    "url": "/porfolio/static/css/main.e3c0b22d.chunk.css"
+  },
+  {
+    "revision": "3da7aa2c8244ed24d860",
+    "url": "/porfolio/static/js/2.3a164514.chunk.js"
+  },
+  {
+    "revision": "85d965a067415930481a1b5c455d0527",
+    "url": "/porfolio/static/js/2.3a164514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16ef27823cf51c2c752",
+    "url": "/porfolio/static/js/main.9804cde5.chunk.js"
+  },
+  {
+    "revision": "29d31c490aaaa8332a31",
+    "url": "/porfolio/static/js/runtime-main.eacfc500.js"
+  },
+  {
+    "revision": "0e204bc245a06fd0c54d2d3bf362e73c",
+    "url": "/porfolio/static/media/ava.0e204bc2.png"
+  },
+  {
+    "revision": "8e1796aca35a24e0d60581c11db8cdd9",
+    "url": "/porfolio/static/media/react.8e1796ac.jpg"
+  },
+  {
+    "revision": "d547c492406c205fecdd4551b46a9237",
+    "url": "/porfolio/static/media/solo.d547c492.jpg"
+  }
+]);
